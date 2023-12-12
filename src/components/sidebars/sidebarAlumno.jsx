0bf303aa20d708ffd7b67dcae8dcafc9b1f90422
Maximiliano <max.borquezc@gmail.com>
2023-12-11 21:57:59 -0300

    correcion boton practica sidebar

--- src/components/sidebars/sidebarAlumno.jsx
+++ src/components/sidebars/sidebarAlumno.jsx
@@ -1,16 +1,14 @@
-
-import React from "react";
-import { List, ListItem, ListItemText } from "@mui/material";
+import React, { useState } from "react";
+import { List, ListItem, ListItemText, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "react-query";
 import clienteAxios from "../../helpers/clienteaxios";
-import { Output } from "@mui/icons-material";
+import { Output, ArrowForward, ArrowDropDown } from "@mui/icons-material";
 import Swal from "sweetalert2";
 
 const SidebarAlumno = () => {
-
-    const [, setOpen] = React.useState(false);
-    const navigate = useNavigate();
+  const [showPracticeOptions, setShowPracticeOptions] = useState(false);
+  const navigate = useNavigate();
 
   const id_alumno = localStorage.getItem("id_alumno");
   const comprobar = useQuery("update_inscripcion", async () => {
@@ -18,11 +16,13 @@ const SidebarAlumno = () => {
       id_alumno: id_alumno,
     });
     if (response.status === 200) {
-      localStorage.setItem("id_inscripcion_practica", response.data.id_inscripcion);
+      localStorage.setItem(
+        "id_inscripcion_practica",
+        response.data.id_inscripcion
+      );
       return response.data;
     }
   });
-
 
   const logout = () => {
     setOpen(false);
@@ -52,19 +52,60 @@ const SidebarAlumno = () => {
       <ListItem button onClick={() => navigate("/perfil")}>
         <ListItemText sx={{ textAlign: "center" }} primary="Perfil" />
       </ListItem>
-      {comprobar.status === "success" && comprobar.data.inscrito_sistema === true && (
-        <ListItem button onClick={() => navigate(`/showbitalumno/${comprobar.data.id_inscripcion}`)}>
-          <ListItemText sx={{ textAlign: "center" }} primary="Bitácoras" />
-        </ListItem>
-      )}
       <ListItem button onClick={() => navigate("/aptitudes")}>
         <ListItemText sx={{ textAlign: "center" }} primary="Aptitudes" />
       </ListItem>
-      <ListItem button onClick={logout} >
-       <ListItemText  sx={{textAlign:"center",display:"flex",flexWrap:"nowrap",justifyContent:"center",alignItems:"center"}}>
-      <p> Cerrar Sesión</p> <Output/>
-       </ListItemText>
-        </ListItem>
+
+      {/* Botón 'Práctica' */}
+      <ListItem>
+        <Button
+          onClick={() => setShowPracticeOptions(!showPracticeOptions)}
+          sx={{
+            width: "100%",
+            justifyContent: "center",
+            textTransform: "none",
+            color: "white",
+          }}
+        >
+          Práctica < ArrowDropDown />
+        </Button>
+      </ListItem>
+
+      {showPracticeOptions && (
+        <>
+          {comprobar.status === "success" &&
+        comprobar.data.inscrito_sistema === true && (
+          <ListItem
+            button
+            onClick={() =>
+              navigate(`/showbitalumno/${comprobar.data.id_inscripcion}`)
+            }
+          >
+            <ListItemText sx={{ textAlign: "center" }} primary="- Bitácoras" />
+          </ListItem>
+        )}
+          <ListItem button onClick={() => navigate("/alumno")}>
+          <ListItemText sx={{ textAlign: "center"}}  primary="- Informe" />
+          </ListItem>
+          <ListItem button onClick={() => navigate("/alumno")}>
+          <ListItemText sx={{ textAlign: "center"}} primary="- Evaluacion" />
+          </ListItem>
+        </>
+      )}
+
+      <ListItem button onClick={logout}>
+        <ListItemText
+          sx={{
+            textAlign: "center",
+            display: "flex",
+            flexWrap: "nowrap",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p> Cerrar Sesión</p> <Output />
+        </ListItemText>
+      </ListItem>
     </List>
   );
 };
