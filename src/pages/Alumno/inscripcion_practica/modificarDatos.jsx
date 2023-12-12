19900f576e387df572c63a81d598d3141871151e
Maximiliano <max.borquezc@gmail.com>
2023-12-12 00:52:51 -0300

    sidebar fija mod inscripcion

--- src/pages/Alumno/inscripcion_practica/modificarDatos.jsx
+++ src/pages/Alumno/inscripcion_practica/modificarDatos.jsx
@@ -1,18 +1,31 @@
-import { Grid, Typography } from "@mui/material"
-import HeaderAlumno from "../../../components/headers/headerAlumno";
-import FormularioModificarInscripcion from "./components/formularioModificarInscripcion";
+import React from 'react';
+import { Grid } from '@mui/material';
+import HeaderAlumno from '../../../components/headers/headerAlumno';
+import FormularioModificarInscripcion from './components/formularioModificarInscripcion';
+import SidebarAlumno from '../../../components/sidebars/sidebarAlumno';
 
-
-
-const ModificarDatos = ()=>{
-    
+const ModificarDatos = () => {
     return (
-        <Grid sx={{width:"100%",display:"flex",flexDirection:"column"}}>
-            <HeaderAlumno/>
-            
-            <FormularioModificarInscripcion/>
+        <Grid container sx={{ width: '100%', minHeight: '100vh', flexDirection: 'column' }}>
+            {/* Header */}
+            <Grid item>
+                <HeaderAlumno />
+            </Grid>
+
+            {/* Main content area */}
+            <Grid item container sx={{ flex: 1 }}>
+                {/* Sidebar on the left */}
+                <Grid item xs={3}>
+                    <SidebarAlumno />
+                </Grid>
+
+                {/* FormularioModificarInscripcion component */}
+                <Grid item xs={9} sx={{ paddingLeft: 2 }}>
+                    <FormularioModificarInscripcion />
+                </Grid>
+            </Grid>
         </Grid>
-    )
-}
+    );
+};
 
 export default ModificarDatos;
